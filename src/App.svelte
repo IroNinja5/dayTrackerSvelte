4d65aca12000e23347517f0065dc2b717ce73455
IroNinja5 <colinvandermeer@gmail.com>
2022-06-25 00:19:43 -0400

    Remove unused assets and code

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
   import Tracker from './lib/Tracker.svelte'
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
   <Tracker name={"Move"}/>
 </main>
 
@@ -20,36 +16,5 @@
     text-align: center;
     padding: 1em;
     margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
   }
 </style>
